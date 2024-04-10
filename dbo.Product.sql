7e7b3f3c2ba34bf858f65249081323c3600c6c26
Prateek <prateek.9n@gmail.com>
2024-04-10 15:57:21 -0400

    Worked on DB Product table and added columns

---
+++ dbo.Product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [ProductId]   INT           IDENTITY (1, 1) NOT NULL,
+    [ProductName] VARCHAR (255) NOT NULL,
+    [Description] VARCHAR(255) NOT NULL, 
+    [UnitPrice] FLOAT NOT NULL, 
+    [CategoryID] INT NOT NULL, 
+    [SubCategoryID] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([ProductId] ASC)
+);
+
