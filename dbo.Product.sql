5d2dd202f842bd43608baaf9279ee86319e08b20
Prateek <prateek.9n@gmail.com>
2024-04-11 12:11:08 -0400

    working on category table in db

--- dbo.Product.sql
+++ dbo.Product.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[Product] (
-    [ProductId]   INT           IDENTITY (1, 1) NOT NULL,
-    [ProductName] VARCHAR (255) NOT NULL,
-    [Description] VARCHAR(255) NOT NULL, 
-    [UnitPrice] FLOAT NOT NULL, 
-    [CategoryID] INT NOT NULL, 
-    [SubCategoryID] INT NOT NULL, 
-    PRIMARY KEY CLUSTERED ([ProductId] ASC)
+    [ProductId]     INT           IDENTITY (1, 1) NOT NULL,
+    [ProductName]   VARCHAR (255) NOT NULL,
+    [ProductDescription]   VARCHAR (255) NULL,
+    [ProductUnitPrice]     FLOAT (53)    NULL,
+    [ProductCategoryID]    INT           NOT NULL,
+    [ProductSubCategoryID] INT           NULL,
+    PRIMARY KEY CLUSTERED ([ProductId] ASC), 
+    CONSTRAINT [FK_Product_ToTable] FOREIGN KEY (ProductCategoryID) REFERENCES ProductCategory(ProductCategoryID)
 );
 
